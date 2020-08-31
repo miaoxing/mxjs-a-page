e3ec60de5f86c2211edc4ce4fd8d06d768dfff7a
twinh <twinhuang@qq.com>
2020-08-31 15:29:34 +0800

    chore: 更改 breadcrumb 读取后端新接口

--- Page.js
+++ Page.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import {Breadcrumb} from '@mxjs/bootstrap';
-import app from '@mxjs/app';
+import app, {Router} from '@mxjs/app';
 import {LinkContainer} from 'react-router-bootstrap';
 import {Box} from 'rebass';
-import http from '@mxjs/http';
 import {invisible} from '@mxjs/css';
 import '@mxjs/bootstrap-antd/Breadcrumb/style';
 
@@ -17,22 +16,37 @@ class Page extends React.Component {
     breadcrumb: [],
   };
 
-  componentDidMount() {
-    if (this.props.breadcrumb !== false) {
-      http.get('admin-page/breadcrumb', {
-        params: {
-          ctrl: app.namespace + '/' + app.controller,
-          act: app.action,
-        },
-      })
-        .then(ret => {
-          if (ret.code !== 1) {
-            // 忽略错误未找到的情况
-            return;
-          }
-          this.setState({breadcrumb: ret.data});
-        });
+  constructor(props) {
+    super(props);
+
+    this.router = new Router();
+    this.router.setPages(miaoxing.pages);
+  }
+
+  getBreadcrumb() {
+    const page = this.router.match(window.location.pathname);
+    if (!page) {
+      return [];
     }
+
+    const breadcrumb = [];
+    let url = '';
+    let next = miaoxing.pages;
+    page.paths.forEach(path => {
+      url += path;
+      next = next[path];
+
+      // 忽略变量
+      if (path.includes('[')) {
+        return;
+      }
+
+      breadcrumb.push({
+        name: next.name,
+        url: url.includes('[') ? null : app.url(url.substr(1)), // 移除 '/' 前缀
+      });
+    });
+    return breadcrumb;
   }
 
   renderBreadcrumb() {
@@ -44,6 +58,8 @@ class Page extends React.Component {
       return '';
     }
 
+    const items = this.getBreadcrumb();
+
     return <Box mt={-4} mx={-4} mb={4} bg="white"
       sx={{
         '.breadcrumb': {
@@ -53,13 +69,15 @@ class Page extends React.Component {
       }}
     >
       <Breadcrumb>
-        {this.state.breadcrumb.map((breadcrumb, index) => (
-          <LinkContainer key={breadcrumb.name} to={breadcrumb.url}>
-            <Breadcrumb.Item active={this.state.breadcrumb.length === index + 1}>
-              {breadcrumb.name}
+        {items.map((item, index) => {
+          const Child = item.url ? LinkContainer : React.Fragment;
+          const props = item.url ? {to: item.url} : {};
+          return <Child key={item.name} {...props}>
+            <Breadcrumb.Item active={items.length === index + 1}>
+              {item.name}
             </Breadcrumb.Item>
-          </LinkContainer>
-        ))}
+          </Child>
+        })}
         {this.state.breadcrumb.length === 0 && <Breadcrumb.Item css={invisible()}>...</Breadcrumb.Item>}
       </Breadcrumb>
     </Box>;
