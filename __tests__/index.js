5022916114c25e487e7b05a16cc4f59efb6cfc75
twinh <twinhuang@qq.com>
2021-06-02 13:39:32 +0800

    test(a-page, Page): 面包屑忽略中间的空白路径

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import {Page} from '..';
+import {Page, PageContext} from '..';
 import {render} from '@testing-library/react';
+import {setUrl, resetUrl} from '@mxjs/test';
 
 describe('Page', () => {
   test('basic', () => {
     const result = render(<Page/>);
     expect(result.container).toMatchSnapshot();
+  });
+
+  test('breadcrumb ignore empty path name', () => {
+    setUrl('/admin/test/child');
+
+    const result = render(<PageContext.Provider value={{
+      pages: {
+        '/admin': {
+          name: 'Home',
+          '/test': {
+            '/child': {
+              name: 'Child Page',
+            },
+          },
+        },
+      },
+    }}>
+      <Page/>
+    </PageContext.Provider>);
+
+    expect(result.container).toMatchSnapshot();
+
+    resetUrl();
   });
 });
 
