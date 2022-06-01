3c6502859179528e83718c413bd46833b2bb235e
twinh <twinhuang@qq.com>
2022-06-01 10:54:36 +0800

    test(a-page): 更新测试以支持 `hash` 路由器模式

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,38 +1,42 @@
 import {Page, PageContext} from '..';
 import {render} from '@testing-library/react';
-import {setUrl, resetUrl} from '@mxjs/test';
+import {MemoryRouter} from 'react-router';
 
 describe('Page', () => {
   test('basic', () => {
-    const result = render(<Page/>);
+    const result = render(<MemoryRouter>
+      <Page/>
+    </MemoryRouter>);
     expect(result.container).toMatchSnapshot();
   });
 
   test('breadcrumb ignore empty path name', () => {
-    setUrl('/admin/test/child');
-
-    const result = render(<PageContext.Provider value={{
-      pages: {
-        '/admin': {
-          name: 'Home',
-          '/test': {
-            '/child': {
-              name: 'Child Page',
+    const result = render(<MemoryRouter
+      initialEntries={['/admin/test/child']}
+    >
+      <PageContext.Provider value={{
+        pages: {
+          '/admin': {
+            name: 'Home',
+            '/test': {
+              '/child': {
+                name: 'Child Page',
+              },
             },
           },
         },
-      },
-    }}>
-      <Page/>
-    </PageContext.Provider>);
+      }}>
+        <Page/>
+      </PageContext.Provider>
+    </MemoryRouter>);
 
     expect(result.container).toMatchSnapshot();
-
-    resetUrl();
   });
 
   test('page with css props', () => {
-    const {container} = render(<Page mb={2}/>);
+    const {container} = render(<MemoryRouter>
+      <Page mb={2}/>
+    </MemoryRouter>);
 
     expect(container).toMatchSnapshot();
   });
