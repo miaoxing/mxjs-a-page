b966a53236fcc2f259b96e6f8b5963579d6ca6ef
twinh <twinhuang@qq.com>
2023-05-31 23:02:25 +0800

    test(antd5): 关闭 `hashed`

--- __tests__/index.js
+++ __tests__/index.js
@@ -2,46 +2,59 @@ import {Page, PageContext} from '..';
 import {render} from '@testing-library/react';
 import {MemoryRouter} from 'react-router';
 import {bootstrap} from '@mxjs/test';
+import {ConfigProvider} from 'antd';
 
 bootstrap();
 
 describe('Page', () => {
   test('basic', () => {
-    const result = render(<MemoryRouter>
-      <Page/>
-    </MemoryRouter>);
+    const result = render(
+      <ConfigProvider theme={{hashed: false}}>
+        <MemoryRouter>
+          <Page/>
+        </MemoryRouter>
+      </ConfigProvider>
+    );
     expect(result.container).toMatchSnapshot();
   });
 
   test('breadcrumb match menu', () => {
-    const result = render(<MemoryRouter
-      initialEntries={['/admin/test/child']}
-    >
-      <PageContext.Provider value={{
-        menus: [
-          {
-            url: 'admin/test',
-            label: 'Test Page',
-            children: [
+    const result = render(
+      <ConfigProvider theme={{hashed: false}}>
+        <MemoryRouter
+          initialEntries={['/admin/test/child']}
+        >
+          <PageContext.Provider value={{
+            menus: [
               {
-                url: 'admin/test/child',
-                label: 'Child Page',
+                url: 'admin/test',
+                label: 'Test Page',
+                children: [
+                  {
+                    url: 'admin/test/child',
+                    label: 'Child Page',
+                  },
+                ],
               },
             ],
-          },
-        ],
-      }}>
-        <Page/>
-      </PageContext.Provider>
-    </MemoryRouter>);
+          }}>
+            <Page/>
+          </PageContext.Provider>
+        </MemoryRouter>
+      </ConfigProvider>
+    );
 
     expect(result.container).toMatchSnapshot();
   });
 
   test('page with css props', () => {
-    const {container} = render(<MemoryRouter>
-      <Page mb={2}/>
-    </MemoryRouter>);
+    const {container} = render(
+      <ConfigProvider theme={{hashed: false}}>
+        <MemoryRouter>
+          <Page mb={2}/>
+        </MemoryRouter>
+      </ConfigProvider>
+    );
 
     expect(container).toMatchSnapshot();
   });
