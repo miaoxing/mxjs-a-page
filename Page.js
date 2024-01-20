6ff2ac707ba3397e5dcac152ee84ef803df0a88c
twinh <twinhuang@qq.com>
2024-01-20 10:33:50 +0800

    feat(a-page): `Page` 组件子元素增加默认 gap

--- Page.js
+++ Page.js
@@ -1,11 +1,11 @@
 import $ from 'miaoxing';
-import {useState, useEffect} from 'react';
-import { Box } from '@chakra-ui/react';
-import {Breadcrumb as AntdBreadcrumb} from 'antd';
-import {useLocation, Link} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Box } from '@mxjs/a-box';
+import { Breadcrumb as AntdBreadcrumb } from 'antd';
+import { useLocation, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import {usePageContext} from './PageContext';
-import {matchMenus} from './menus';
+import { usePageContext } from './PageContext';
+import { matchMenus } from './menus';
 
 const hasVar = (path) => {
   return path.includes('[');
@@ -55,32 +55,27 @@ const Breadcrumb = () => {
   }, [page.menus.length]);
 
   return (
-    <Box mt={-4} mx={-4}>
-      {/* 异步加载时先占好位置 */}
-      <Box as={AntdBreadcrumb} py={3} pl={4} minH="46px">
-        {items.map((item) => {
-          return <AntdBreadcrumb.Item key={item.label}>
-            {item.url ? <Link to={item.url}>{item.label}</Link> : item.label}
-          </AntdBreadcrumb.Item>;
-        })}
-      </Box>
+    <Box as={AntdBreadcrumb} minH="22px"> {/* 异步加载时先占好位置 */}
+      {items.map((item) => {
+        return <AntdBreadcrumb.Item key={item.label}>
+          {item.url ? <Link to={item.url}>{item.label}</Link> : item.label}
+        </AntdBreadcrumb.Item>;
+      })}
     </Box>
   );
 };
 
 const Page = ({raw, breadcrumb, ...props}) => {
   return (
-    <>
+    <Box display="flex" gap={4} flexDirection="column">
       {renderBreadcrumb(breadcrumb)}
-      <Box
-        p={raw ? 0 : 4}
-        bg={raw ? null : 'white'}
-        rounded="lg"
-        {...props}
-      >
-        {props.children}
-      </Box>
-    </>
+      {
+        raw ? props.children :
+          <Box p={4} bg="white" rounded="lg" {...props}>
+            {props.children}
+          </Box>
+      }
+    </Box>
   );
 };
 
